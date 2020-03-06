20d12e8a91d31934818be3609bb10a09d0249a5e
ludonx <tekamludovik@yahoo.fr>
2020-03-06 17:59:22 +0100

    client-server two way

--- client_serveur/Client.java
+++ client_serveur/Client.java
@@ -1,34 +1,56 @@
 
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.net.UnknownHostException;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 
 public class Client {
 
 	public static void main(String[] args) throws UnknownHostException, IOException {
-		String ip="localhost";
-		int port = 9999; //0-1023 to 65535
-		Socket clientSocket = new Socket(ip,port);
-		PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
-		BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-		String str_send = "Bonjour je suis le client";
+		// Open your connection to a server, at port 1254
+		Socket socketClient = null;
+		DataInputStream dataInputStream = null;
+		DataOutputStream dataOutputStream = null;
+		Scanner scanner = null;
+		String msg_end = "bye";
+		try {
+			socketClient = new Socket("127.0.0.1",1254);
+			String messageFromServer;
+			String messageToServer;
+			do {
+				dataInputStream = new DataInputStream(socketClient.getInputStream());
+				messageFromServer = new String (dataInputStream.readUTF());
+				System.out.println("server : " + messageFromServer);
+				// When done, just close the connection and exit
+				System.out.print("> ");
+				scanner = new Scanner(System.in);
+				messageToServer = scanner.nextLine();
+				dataOutputStream = new DataOutputStream(socketClient.getOutputStream());
+				dataOutputStream.writeUTF(messageToServer);
+				dataOutputStream.flush();
+			//} while(!messageFromServer.equals(msg_end)); // stop when server said bye
+			} while(!messageToServer.equals(msg_end)); // stop when client said bye
 		
-		out.write(str_send);
-		out.flush();
+		// Managing exception
+		} catch (ConnectException e) {
+			System.err.println("Error: "+e.getMessage());
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.err.println("Error: "+e.getMessage());
+			e.printStackTrace();
 		
-		//String str_recev = in.readLine();
-		//System.out.println("C: Data from Server " + str_recev);
 		
-		out.close();
-		clientSocket.close();
-
+		
+		
+		// Closing socket
+		} finally {
+			dataOutputStream.close();
+			scanner.close();
+			dataInputStream.close();
+			socketClient.close();
+			System.out.print("[Connection closed] ");
+		}
 	}
 
 }
--- client_serveur/Server.java
+++ client_serveur/Server.java
@@ -1,39 +1,62 @@
 
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 public class Server {
 
 	public static void main(String[] args) throws IOException {
+		DataOutputStream dataOutputStream = null;
+		DataInputStream dataInputStream = null;		
+		// Register service on port 1254
+		ServerSocket serverSocket = null;
+		Socket socketClient = null;
+		Scanner scanner = null;
+		String msg_end = "bye";
 		
-		System.out.println("S: Server is started");
-		ServerSocket serverSocket = new ServerSocket(9999);
+		try {
+			serverSocket = new ServerSocket(1254);
+			System.out.println("Server is running...");
+			socketClient = serverSocket.accept();
+			String messageFromClient;
+			String messageToClient;
+			dataOutputStream = new DataOutputStream(socketClient.getOutputStream());
+			dataOutputStream.writeUTF("Hi There");
+			do {			
+				dataInputStream = new DataInputStream(socketClient.getInputStream());
+				messageFromClient = new String (dataInputStream.readUTF());
+				System.out.println("client : " + messageFromClient);
+				System.out.print("> ");
+				
+				scanner = new Scanner(System.in);
+				messageToClient = scanner.nextLine();
+				dataOutputStream.writeUTF(messageToClient);
+				dataOutputStream.flush();
+				
+				
+			//} while(!messageToClient.equals(msg_end));//close when server said bye
+			} while(!messageFromClient.equals(msg_end));//close when client said bye
+			// Close the connection, but not the server socket
+			System.out.print("[client said : bye] ");
+		// Managing exception
+		} catch (ConnectException e) {
+			System.err.println("Error: "+e.getMessage());
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.err.println("Error: "+e.getMessage());
+			e.printStackTrace();
 		
-		System.out.println("S: Server is waiting for client request");
-		Socket clientSocket = serverSocket.accept();
-		PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
-		BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-		System.out.println("S: Client Connected");
 		
-		String str = in.readLine();
-		
-		System.out.println("S: Client Data : "+ str);
-		
-		
-		// String response = "[Bonjour client]";
-		// out.write(response);
-		// out.flush();
-		// System.out.println("S: Response sent to client "+ str);
-		
-		out.close();
-		serverSocket.close();
+		// Closing socket
+		} finally {
+			dataOutputStream.close();
+			dataInputStream.close();
+			scanner.close();
+			socketClient.close();
+			serverSocket.close();
+			System.out.print("[Connection closed] ");
+		}
 	}
 
 }

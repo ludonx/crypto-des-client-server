b18aa2bb462b82a07b25520dcfb912bf93fd73e4
ludonx <tekamludovik@yahoo.fr>
2020-03-07 16:53:58 +0100

    Début de session prototype

--- client_serveur/Client.java
+++ client_serveur/Client.java
@@ -2,6 +2,7 @@
 
 import java.io.*;
 import java.net.*;
+import java.nio.file.*;
 import java.util.*;
 
 
@@ -13,22 +14,54 @@ public class Client {
 		DataInputStream dataInputStream = null;
 		DataOutputStream dataOutputStream = null;
 		Scanner scanner = null;
+
+		// --------------------------------//
+		String filePathToPublicKeyServer = "disk/client/server_publique.pub.pem";
+
+		// --------------------------------//
+
 		String msg_end = "bye";
+		int nbr_msg = 0;
+		String msg_init_session = "HELLO";
 		try {
 			socketClient = new Socket("127.0.0.1",1254);
 			String messageFromServer;
 			String messageToServer;
+			
 			do {
-				dataInputStream = new DataInputStream(socketClient.getInputStream());
-				messageFromServer = new String (dataInputStream.readUTF());
-				System.out.println("server : " + messageFromServer);
 				// When done, just close the connection and exit
-				System.out.print("> ");
-				scanner = new Scanner(System.in);
-				messageToServer = scanner.nextLine();
+				if(nbr_msg == 0){
+					dataInputStream = new DataInputStream(socketClient.getInputStream());
+					messageFromServer = new String (dataInputStream.readUTF());
+					System.out.println("server : " + messageFromServer);
+
+					System.out.println("[... envoie du message["+msg_init_session+"] de debut de session ...]");
+					messageToServer = msg_init_session;
+				}else if(nbr_msg == 1){
+					dataInputStream = new DataInputStream(socketClient.getInputStream());
+					messageFromServer = new String (dataInputStream.readUTF());
+					System.out.println("server : " + messageFromServer);
+
+					System.out.println("[... sauvegarde de la clé plublique du server ...]");
+    				Files.write(Paths.get(filePathToPublicKeyServer), messageFromServer.getBytes());
+
+					System.out.println("[... génération de la clé DES ...]");
+					System.out.println("[... chiffrement de la clé DES en RSA avec la clé publique du serveur ...]");
+					System.out.println("[... envoie de la clé secrète chiffré ...]");
+					messageToServer = "voici ma clés secrète chiffré...";
+				}else{
+					dataInputStream = new DataInputStream(socketClient.getInputStream());
+					messageFromServer = new String (dataInputStream.readUTF());
+					System.out.println("server : " + messageFromServer);
+					System.out.print("> ");
+					scanner = new Scanner(System.in);
+					messageToServer = scanner.nextLine();
+				}
+				
 				dataOutputStream = new DataOutputStream(socketClient.getOutputStream());
 				dataOutputStream.writeUTF(messageToServer);
 				dataOutputStream.flush();
+				nbr_msg = nbr_msg + 1;
 			//} while(!messageFromServer.equals(msg_end)); // stop when server said bye
 			} while(!messageToServer.equals(msg_end)); // stop when client said bye
 		
--- client_serveur/Server.java
+++ client_serveur/Server.java
@@ -1,7 +1,7 @@
 
-
 import java.io.*;
 import java.net.*;
+import java.nio.file.*;
 import java.util.*;
 
 public class Server {
@@ -13,8 +13,16 @@ public class Server {
 		ServerSocket serverSocket = null;
 		Socket socketClient = null;
 		Scanner scanner = null;
+
+		// --------------------------------//
+
+		String filePathToPublicKey = "disk/server/publique.pub.pem";
+		//filePathToPublicKey = "/home/ludovik/Bureau/ING3/crypto/crypto-des-client-server/client_serveur/disk/server/test.txt";
+		// --------------------------------//
 		String msg_end = "bye";
-		
+		int nbr_msg = 0;
+		String msg_init_session = "HELLO";
+		// --------------------------------//
 		try {
 			serverSocket = new ServerSocket(1254);
 			System.out.println("Server is running...");
@@ -27,13 +35,22 @@ public class Server {
 				dataInputStream = new DataInputStream(socketClient.getInputStream());
 				messageFromClient = new String (dataInputStream.readUTF());
 				System.out.println("client : " + messageFromClient);
-				System.out.print("> ");
 				
-				scanner = new Scanner(System.in);
-				messageToClient = scanner.nextLine();
+				if(messageFromClient.equals(msg_init_session)){
+					System.out.println("[... envoie de ma clé publique ...]");
+					messageToClient = readFile( filePathToPublicKey );
+				}else if(nbr_msg == 1){
+					System.out.println("[... clé secrète chiffré reçu ...]");
+					messageToClient = readFile( filePathToPublicKey );
+
+				}else{
+					System.out.print("> ");
+					scanner = new Scanner(System.in);
+					messageToClient = scanner.nextLine();
+				}
 				dataOutputStream.writeUTF(messageToClient);
 				dataOutputStream.flush();
-				
+				nbr_msg = nbr_msg + 1;
 				
 			//} while(!messageToClient.equals(msg_end));//close when server said bye
 			} while(!messageFromClient.equals(msg_end));//close when client said bye
@@ -58,5 +75,22 @@ public class Server {
 			System.out.print("[Connection closed] ");
 		}
 	}
+
+
+	private static String readFile(String filePath) 
+    {
+        String content = "";
+ 
+        try
+        {
+            content = new String ( Files.readAllBytes( Paths.get(filePath) ) );
+        } 
+        catch (IOException e) 
+        {
+            e.printStackTrace();
+        }
+ 
+        return content;
+    }
 
 }
